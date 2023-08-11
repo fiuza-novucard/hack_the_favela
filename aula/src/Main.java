45886b6ed7ecf4f0666aedef2aad7338b2689b4c
Frederico Fiuza <frederico.fiuza@altbank.co>
2023-08-11 20:49:13 -0300

    Código de calculo de idade

--- aula/src/Main.java
+++ aula/src/Main.java
@@ -1,5 +1,10 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Pessoa pessoa = new Pessoa("João", LocalDate.of(1990, 1, 1));
+        int idade = pessoa.calcularIdade(2020);
+        System.out.println("Idade: " + idade);
     }
 }
--- aula/src/Pessoa.java
+++ aula/src/Pessoa.java
@@ -1,5 +1,18 @@
 import java.math.BigInteger;
+import java.time.LocalDate;
 
 public class Pessoa {
+
+    private String nome;
+    private LocalDate dataNascimento;
+
+    public Pessoa(String nome, LocalDate dataNascimento) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int calcularIdade( int ano) {
+        return ano - dataNascimento.getYear();
+    }
 
 }

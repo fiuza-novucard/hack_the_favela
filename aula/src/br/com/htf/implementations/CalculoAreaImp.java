abb961f27e952d9711db8c2968f4d6eba96c0083
LeandroRamos <Leandro.leronn@gmail.com>
2023-08-18 19:57:42 -0300

    exercicio

---
+++ aula/src/br/com/htf/implementations/CalculoAreaImp.java
@@ -0,0 +1,19 @@
+package br.com.htf.implementations;
+import br.com.htf.contracts.CalculoArea;
+
+public class CalculoAreaImp implements CalculoArea{
+    @Override
+    public double calcularAreaRetangulo(double base, double altura) {
+        return 0;
+    }
+
+    @Override
+    public double calcularAreaTriangulo(double base, double altura) {
+        return 0;
+    }
+
+    @Override
+    public double calcularAreaCirculo(double raio) {
+        return 0;
+    }
+}

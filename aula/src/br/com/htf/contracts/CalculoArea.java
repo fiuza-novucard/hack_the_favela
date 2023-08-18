1e96a6e46172535529f8fe6ec13e3298541b1c26
Frederico Fiuza <frederico.fiuza@altbank.co>
2023-08-18 19:18:21 -0300

    Interface calcular área

---
+++ aula/src/br/com/htf/contracts/CalculoArea.java
@@ -0,0 +1,7 @@
+package br.com.htf.contracts;
+
+public interface CalculoArea {
+    public double calcularAreaRetangulo(double base, double altura);
+    public double calcularAreaTriangulo(double base, double altura);
+    public double calcularAreaCirculo(double raio);
+}

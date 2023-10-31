16f0e9522e98a617446881f907fd4b2dd2eb9413
LeandroRamos <Leandro.leronn@gmail.com>
2023-10-30 21:17:55 -0300

    30/10

--- src/main/java/com/aulas/spring/conversor/TaxaCambioService.java
+++ src/main/java/com/aulas/spring/conversor/TaxaCambioService.java
@@ -10,5 +10,6 @@ public class TaxaCambioService {
         return 0.0;
     }
 
+
 }
 
--- src/test/java/com/aulas/spring/ConversorMoedaTeste.java
+++ src/test/java/com/aulas/spring/ConversorMoedaTeste.java
@@ -2,10 +2,17 @@ package com.aulas.spring;
 
 import com.aulas.spring.conversor.ConversorMoedaService;
 import com.aulas.spring.conversor.TaxaCambioService;
+import com.aulas.spring.pedido.Pedido;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Service;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class ConversorMoedaTeste {
@@ -16,6 +23,18 @@ public class ConversorMoedaTeste {
     @Mock
     private TaxaCambioService taxaCambioService;
 
+    @Test
+    public void testConversorMoeda(){
+        double valor = 4.5;
+        String MoedaOrigem = "Real";
+        String MoedaDestino = "Dollar";
+        when(taxaCambioService.getTaxaCambio(anyString(),anyString())).thenReturn(5.5);
+        double moedaconvertida = conversorMoedaService.converterMoeda(valor,MoedaOrigem,MoedaDestino);
+        Assertions.assertEquals(24.75,moedaconvertida);
+        verify(taxaCambioService,times(2)).getTaxaCambio(anyString(),anyString());
+    }
+
+    
 
 
 }

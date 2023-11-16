2de0599857bff82b68f338ef7fbab822bbaed457
Frederico Fiuza <frederico.fiuza@altbank.co>
2023-11-16 20:41:33 -0300

    Exercício

---
+++ src/main/java/com/aulas/spring/controllers/DataController.java
@@ -0,0 +1,23 @@
+package com.aulas.spring.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/hora-fuso")
+public class DataController {
+
+    @GetMapping
+    public String getFusoHorario(@RequestBody String fusoHorario) {
+        /**
+         * Enviar a hora de acordo com o país válido
+         * Se enviar nome de país inválido, retornar 400
+         */
+        return LocalDateTime.now().toString();
+    }
+}

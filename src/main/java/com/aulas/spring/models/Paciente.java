1cb9fdb8263e82ba66bd526be48f8915425c7ecf
Leandro Ramos <leandro.leronn@gmail.com>
2023-12-07 20:08:02 -0300

    Criado Entity Paciente e Paciente_imc

---
+++ src/main/java/com/aulas/spring/models/Paciente.java
@@ -0,0 +1,38 @@
+package com.aulas.spring.models;
+
+import jakarta.persistence.*;
+
+@Entity (name = "paciente")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    public Paciente(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Paciente() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}

6ba3e199fb2b987d1274715674944f17e9d52059
Frederico Fiuza <frederico.fiuza@altbank.co>
2023-12-07 20:03:12 -0300

    1 - Registrar o histórico do IMC dos seus pacientes. 2 - Consultar data com maior IMC 3 - Consultar média de peso em determinado perído de tempo 4 - Lista de registros do histórico dos pacientes.

--- src/main/java/com/aulas/spring/controllers/PacienteController.java
+++ src/main/java/com/aulas/spring/controllers/PacienteController.java
@@ -1,7 +1,15 @@
 package com.aulas.spring.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/paciente")
 public class PacienteController {
+    @GetMapping("/listar-historico")
+    public String listarHistorico() {
+        return "Listando histórico";
+    }
+
 }

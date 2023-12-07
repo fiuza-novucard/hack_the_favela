6f1ee24e4e52a4d8d5121a0667fb43375b26d110
Frederico Fiuza <frederico.fiuza@altbank.co>
2023-12-07 20:33:14 -0300

    1 - Registrar o histórico do IMC dos seus pacientes. 2 - Consultar data com maior IMC 3 - Consultar média de peso em determinado perído de tempo 4 - Lista de registros do histórico dos pacientes.

--- src/main/java/com/aulas/spring/controllers/PacienteController.java
+++ src/main/java/com/aulas/spring/controllers/PacienteController.java
@@ -1,5 +1,6 @@
 package com.aulas.spring.controllers;
 
+import com.aulas.spring.models.Paciente_imc;
 import com.aulas.spring.repositories.PacienteHistoricoRepository;
 import com.aulas.spring.repositories.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/paciente")
@@ -20,9 +23,8 @@ public class PacienteController {
     private PacienteRepository pacienteRepository;
 
     @GetMapping("/listar-historico/{id}")
-    public String listarHistorico(@RequestParam(value = "id", defaultValue = "0") Long id) {
-
-        return "Listando histórico";
+    public List<Paciente_imc> listarHistorico(@RequestParam(value = "id", defaultValue = "0") Long id) {
+       return pacienteHistoricoRepository.findByCampoFiltrado(id.intValue());
     }
 
 }
--- src/main/java/com/aulas/spring/repositories/PacienteHistoricoRepository.java
+++ src/main/java/com/aulas/spring/repositories/PacienteHistoricoRepository.java
@@ -2,6 +2,14 @@ package com.aulas.spring.repositories;
 
 import com.aulas.spring.models.Paciente_imc;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PacienteHistoricoRepository extends JpaRepository<Paciente_imc, Long> {
+
+    @Query(value = "SELECT * FROM paciente_imc WHERE id_paciente = :idPaciente", nativeQuery = true)
+    List<Paciente_imc> findByCampoFiltrado(@Param("idPaciente") int idPaciente);
+
 }

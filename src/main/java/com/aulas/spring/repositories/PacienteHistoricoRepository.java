48cbb728d2c4889b2481847dcbc47c99618eedfc
Frederico Fiuza <frederico.fiuza@altbank.co>
2023-12-07 20:17:44 -0300

    1 - Registrar o histórico do IMC dos seus pacientes. 2 - Consultar data com maior IMC 3 - Consultar média de peso em determinado perído de tempo 4 - Lista de registros do histórico dos pacientes.

--- src/main/java/com/aulas/spring/repositories/PacienteHistoricoRepository.java
+++ src/main/java/com/aulas/spring/repositories/PacienteHistoricoRepository.java
@@ -3,5 +3,5 @@ package com.aulas.spring.repositories;
 import com.aulas.spring.models.Paciente_imc;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PacienteHistoricoRepository extends JpaRepository<Paciente_ßimc, Long> {
+public interface PacienteHistoricoRepository extends JpaRepository<Paciente_imc, Long> {
 }
